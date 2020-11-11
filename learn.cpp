483beaa0fa2fcf908a2bce6048ceb1f70c7bf01d
rajantara <rajanpensas@gmail.com>
2020-11-11 22:01:49 +0800

    tugas C++

--- learn.cpp
+++ learn.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
-  return 0;
+  int umur,tahun,hasil;
+
+  cout << "MASUKAN UMUR ANDA: ";
+  cin >> umur;
+  cout << "MASUKAN TAHUN SEKERANG: ";
+  cin >> tahun;
+
+  hasil == umur -  tahun;
+
+  cout << "ANDA LAHIR DI: " << hasil <<endl;
+
 }
+
+// untuk print hasil :  ./a.out
